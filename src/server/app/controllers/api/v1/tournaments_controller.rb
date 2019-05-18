84a660b076b61d61d67bd4c2e23a09cbfa6ff50e
Anson Lam <anson.lam94@gmail.com>
2019-05-17 19:30:00 -0700

    Add tournaments api controller

---
+++ src/server/app/controllers/api/v1/tournaments_controller.rb
@@ -0,0 +1,30 @@
+class Api::V1::TournamentsController < ApplicationController
+before_action :find_tournament, only: [:show, :entries]
+
+  def index  
+    tournaments = Tournament.order(edition: :desc)
+    
+    render(
+      json: tournaments
+    )
+  end
+
+  def show
+    render(
+      json: @tournament
+    )
+  end
+
+  def entries
+  end
+
+  private
+
+  def find_tournament
+    @tournament ||= tournament.find params[:id]
+  end
+
+  # def tournament_params
+  #   params.require(:tournament).permit(:edition, :date)
+  # end
+end
--- src/server/config/routes.rb
+++ src/server/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :tournaments, only: :index do
+          # get '', to: 'tournaments#show'
+          get :entries, on: :member
+      end
+      resources :tournaments, only: :show
+    end
+  end
 end
