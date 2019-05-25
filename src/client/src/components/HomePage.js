3ad894680d04ff6f5cdadf32bbb98c7e5c3e82cd
Anson Lam <anson.lam94@gmail.com>
2019-05-24 18:38:53 -0700

    Add HomePage images to assets

--- src/client/src/components/HomePage.js
+++ src/client/src/components/HomePage.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import styles from '../designs/styles';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import Icon from '@material-ui/core/Icon';
 
 function HomePage() {
   const classes = styles();
   return (
     <div className={classes.homePage}>
-      <h1>2019 BC Secondary School Table Tennis Championships</h1>
+      <Fab color="secondary" variant="extended" aria-label="Edit" className={classes.fab} className={classes.extendedIcon}>
+        Tournaments
+      </Fab>
+      {/* <h1>2019 BC Secondary School Table Tennis Championships</h1> */}
     </div>
   )
 }
