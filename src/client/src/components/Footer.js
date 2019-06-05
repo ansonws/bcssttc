8717783f7cd09014ef7c13fc07da6e66c7b1c2f4
Anson Lam <anson.lam94@gmail.com>
2019-06-04 18:46:13 -0700

    Add sponsor logos to footer

--- src/client/src/components/Footer.js
+++ src/client/src/components/Footer.js
@@ -2,10 +2,46 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer>
-      <p>
-        © 2019 BC Secondary School Table Tennis Championships
-      </p>
+    <footer style={{backgroundColor: 'white'}}>
+    <h1 style={{textAlign: 'center'}}>Thank You to Our Sponsors</h1>
+    <div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          padding: '0'
+        }}
+        >
+        <div 
+          className="sponsorship"
+          style={{ backgroundImage: `url("/images/sponsorships/sunrise.jpg")`}}
+          >
+        </div>
+        <div 
+          className="sponsorship"
+          style={{ backgroundImage: `url("/images/sponsorships/bctta.png" )`}}
+          >
+        </div>
+        <div 
+          className="sponsorship"
+          style={{ backgroundImage: `url("/images/sponsorships/pingpongdepot.jpg")`}}
+          >
+        </div>
+        <div 
+          className="sponsorship"
+          style={{ backgroundImage: `url("/images/sponsorships/flameco.jpg")`}}
+          >
+        </div>
+        <div 
+          className="sponsorship"
+          style={{ backgroundImage: `url("/images/sponsorships/paddle-palace.png")`}}
+          >
+        </div>
+        {/* <p>
+          © 2019 BC Secondary School Table Tennis Championships
+        </p> */}
+      </div>
+    </div>
     </footer>
   )
 }
