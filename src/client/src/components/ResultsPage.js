946f1e870446bd19c52b066e8a4b83809de83057
Anson Lam <anson.lam94@gmail.com>
2019-06-18 16:53:58 -0700

    Add 2019 ResultsPage

--- src/client/src/components/ResultsPage.js
+++ src/client/src/components/ResultsPage.js
@@ -1,89 +1,173 @@
 import React from 'react';
+import { boysTeamStandings, girlsTeamStandings } from '../config';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 function Results(props) {
   return (
     <div>
-<table>
-<tbody>
-<tr>
-<td>&nbsp;<b>Boys Singles:&nbsp;<br />
-<br />
-&nbsp;Gold: Jason Zhang, John Napier Burnett<br />
-&nbsp;Silver: Kenny Jiang, Collingwood<br />
-&nbsp;Bronze: David Lin, Steveston-London<br />
-&nbsp;Bronze: Steve Wang, Sutherland<br />
-<br />
-&nbsp;Boys Doubles:<br />
-<br />
-&nbsp;Gold: Jason Zhang &amp; Andy Chen, John Napier Burnett<br />
-&nbsp;Silver: David Lin &amp; Max Ji, Steveston-London<br />
-&nbsp;Bronze: Allan Zhou &amp; Vincent Liu, Saint George's<br />
-&nbsp;Bronze: Thomas Yuan &amp; Josh Hugh, Saint George's<br />
-<br />
-&nbsp;Boys Team Standings:<br />
-<br />
-&nbsp;Tier 1<br />
-&nbsp;Gold: John Napier Burnett<br />
-&nbsp;Silver: Saint George's A<br />
-&nbsp;Bronze: Semiahmoo A<br />
-<br />
-&nbsp;4. Steveston-London A<br />
-&nbsp;5. Sir Winston Churchill A<br />
-&nbsp;6. Handsworth<br />
-&nbsp;7. Burnaby North A<br />
-&nbsp;8. Collingwood<br />
-&nbsp;9. Samuel Robertson Technical<br />
-&nbsp;10. Gladstone<br />
-&nbsp;11. Britannia<br />
-&nbsp;12. Moscrop<br />
-&nbsp;13. Chilliwack A<br />
-&nbsp;14. Sir Charles Tupper<br />
-&nbsp;15. New Westminster A<br />
-&nbsp;16. Pinetree<br />
-&nbsp;17. Vancouver Technical A<br />
-<br />
-&nbsp;Tier 2<br />
-&nbsp;18. Saint George's B<br />
-&nbsp;19. Sir Winston Churchill B<br />
-&nbsp;20. Steveston-London B<br />
-&nbsp;21. George Elliot<br />
-&nbsp;22. Semiahmoo B<br />
-&nbsp;23. Chilliwack B<br />
-&nbsp;24. Burnaby North B<br />
-&nbsp;25. Vancouver Technical B<br />
-&nbsp;26. New Westminster B<br />
-<br />
-&nbsp;Fair Play Award: Paul Sperber, Semiahmoo</b></td>
-<td>&nbsp;<b>Girls Singles:<br />
-<br />
-&nbsp;Gold: Ivy Liao, Britannia<br />
-&nbsp;Silver: Isabelle Xiong, York House<br />
-&nbsp;Bronze: Benita Zhou, West Point Grey<br />
-&nbsp;Bronze: Laura Lai, York House<br />
-<br />
-&nbsp;Girls Doubles:&nbsp;<br />
-<br />
-&nbsp;Gold: Isabelle Xiong &amp; Laura Lai, York House<br />
-&nbsp;Silver: Ivy Liao &amp; Rose Huang, Britannia<br />
-&nbsp;Bronze: Yanisa Tirapat &amp; Gloria Yin, Burnaby North<br />
-&nbsp;Bronze: Fiona Fan &amp; Jennifer Jiang, Burnaby North<br />
-<br />
-&nbsp;Girls Team Standings:<br />
-<br />
-&nbsp;Gold: York House<br />
-&nbsp;Silver: Burnaby North<br />
-&nbsp;Bronze: Britannia<br />
-<br />
-&nbsp;4. New Westminster<br />
-&nbsp;5. Sir Winston Churchill<br />
-&nbsp;6. Steveston-London<br />
-&nbsp;7. Semiahmoo<br />
-&nbsp;8. Vancouver Technical<br />
-&nbsp;9. Collingwood</b></td>
-</tr>
-</tbody>
-</table>
-</div>
+      <div class="eventTable">
+        <div class="resultsTableDiv">
+          <h2 class="resultsHeading">Boys Singles</h2>
+          <Table class="resultsTable">
+            <TableRow>
+              <TableCell style={{textAlign: "right"}}>
+              <img 
+                  src="images/icons/gold-medal.svg"
+                  width="25px"
+                  height="25px" 
+              />
+              </TableCell>
+              <TableCell>Edison Huang, Lord Byng</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell style={{textAlign: "right"}}>  
+              <img 
+                  src="images/icons/silver-medal.svg"
+                  width="25px"
+                  height="25px" 
+              />
+              </TableCell>
+              <TableCell>Jason Zhang, J. N. Burnett</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell style={{textAlign: "right"}}>  
+              <img 
+                  src="images/icons/bronze-medal.svg"
+                  width="25px"
+                  height="25px" 
+              />
+              </TableCell>
+              <TableCell>Max Ji, Steveston-London & Andy Chen, J. N. Burnett</TableCell>
+            </TableRow>
+          </Table>
+        </div>
+        <div class="resultsTableDiv">
+          <h2 class="resultsHeading">Girls Singles</h2>
+          <Table class="resultsTable">
+            <TableRow>
+                <TableCell style={{textAlign: "right"}}>
+                <img 
+                    src="images/icons/gold-medal.svg"
+                    width="25px"
+                    height="25px" 
+                />
+              </TableCell>
+              <TableCell>Isabelle Xiong, York House</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell style={{textAlign: "right"}}>  
+              <img 
+                  src="images/icons/silver-medal.svg"
+                  width="25px"
+                  height="25px" 
+              />
+              </TableCell>
+              <TableCell>Ivy Liao, Britannia</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell style={{textAlign: "right"}}>  
+                <img 
+                    src="images/icons/bronze-medal.svg"
+                    width="25px"
+                    height="25px" 
+                />
+              </TableCell>
+              <TableCell>Rose Huang, Britannia & Chuchu Lai, Columbia Academy</TableCell>
+            </TableRow>
+          </Table>
+        </div>
+      </div>
+      <div class="eventTable">
+        <div class="resultsTableDiv">
+        <h2>Boys Team Standings</h2>
+        <Table class="resultsTable">
+          <TableRow>
+            <TableCell style={{textAlign: "right"}}>
+              <img 
+                src="images/icons/gold-medal.svg"
+                width="25px"
+                height="25px" 
+              />
+            </TableCell>
+            <TableCell>J. N. Burnett</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell style={{textAlign: "right"}}>
+              <img 
+                  src="images/icons/silver-medal.svg"
+                  width="25px"
+                  height="25px" 
+              />
+              </TableCell>
+            <TableCell>St. George's A</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell style={{textAlign: "right"}}> 
+              <img 
+                  src="images/icons/bronze-medal.svg"
+                  width="25px"
+                  height="25px" 
+              />
+              </TableCell>
+            <TableCell>Burnaby North A</TableCell>
+          </TableRow>
+          {boysTeamStandings.map((team, index) => (
+            <TableRow>
+              <TableCell style={{textAlign: "right"}}>{index + 4}.</TableCell>
+              <TableCell>{team}</TableCell>
+            </TableRow>
+          ))}
+        </Table>
+        </div>
+      <div class="resultsTableDiv">
+        <h2>Girls Team Standings</h2>
+        <Table class="resultsTable">
+          <TableRow>
+            <TableCell style={{textAlign: "right"}}>
+              <img 
+                  src="images/icons/gold-medal.svg"
+                  width="25px"
+                  height="25px" 
+              />
+            </TableCell>
+            <TableCell>J. N. Burnett</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell style={{textAlign: "right"}}>
+              <img 
+                  src="images/icons/silver-medal.svg"
+                  width="25px"
+                  height="25px" 
+              />
+              </TableCell>
+            <TableCell>St. George's A</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell style={{textAlign: "right"}}>
+              <img 
+                  src="images/icons/bronze-medal.svg"
+                  width="25px"
+                  height="25px" 
+              />
+              </TableCell>
+            <TableCell>Burnaby North A</TableCell>
+          </TableRow>
+          {girlsTeamStandings.map((team, index) => (
+            <TableRow>
+              <TableCell style={{textAlign: "right"}}>{index + 4}.</TableCell>
+              <TableCell>{team}</TableCell>
+            </TableRow>
+          ))}
+        </Table>  
+      </div>
+    </div>
+    <div style={{margin: '5em', textAlign: 'center'}}>Icons made by <a href="https://www.flaticon.com/authors/pixel-buddha" title="Pixel Buddha">Pixel Buddha</a> from <a href="https://www.flaticon.com/" 		    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 		    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+    </div>
+
   );
 }
 
--- src/client/src/config.js
+++ src/client/src/config.js
@@ -1,1 +1,33 @@
 export const BASE_URL = `http://localhost:3000/api/v1`;
+export const boysTeamStandings = [
+  'Steveston-London A',
+  'Kitsilano A',
+  'Prince of Wales',
+  'Semiahmoo A',
+  'Sir Winston Churchill A',
+  'Gleneagle',
+  'Moscrop',
+  'R. A. McMath A',
+  'University Hill',
+  'Britannia',
+  'New Westminster',
+  'Vancouver Technical A',
+  'St. George\'s B',
+  'Burnaby North B',
+  'Kitsliano B',
+  'Sir Winston Churchill B',
+  'Semiahmoo B',
+  'Vancouver Technical B',
+  'J. N. Burnett B',
+  'Steveston-London B',
+  'R. A. McMath B'
+];
+export const girlsTeamStandings = [
+  'University Hill',
+  'New Westminster',
+  'Sir Winston Churchill',
+  'Moscrop',
+  'Burnaby North B',
+  'Vancouver Technical A',
+  'Vancouver Technical B',
+];
