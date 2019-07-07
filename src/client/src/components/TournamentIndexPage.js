a5a57f5935127088317a236df7f92170fd9f5a53
Anson Lam <anson.lam94@gmail.com>
2019-07-07 16:37:32 -0700

    Add linear progress bar to TournamentIndexPage

--- src/client/src/components/TournamentIndexPage.js
+++ src/client/src/components/TournamentIndexPage.js
@@ -1,23 +1,32 @@
 import React, {Component} from 'react';
 import Tournament from "../api/tournament";
 import ListDividers from './ListDividers';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 class TournamentIndexPage extends Component {
   state = {
     tournaments: [],
+    isLoading: true,
   }
 
   componentDidMount() {
     Tournament.index().then(tournaments => {
-      this.setState({ tournaments });
+      this.setState({ 
+        tournaments,
+        isLoading: false,
+      });
     });
   }
 
   render() {
-      const {tournaments} = this.state;
-      return (
-        <ListDividers tournaments={tournaments} />
-      );
+    const { tournaments, isLoading } = this.state;
+    if (isLoading) {
+      return <LinearProgress color="primary" variant="query" />
+    }
+    
+    return (
+      <ListDividers tournaments={tournaments} />
+    );
   }
 }
 
