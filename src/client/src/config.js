5ff4167759ef878020338e9e74b15ce815a87dda
Anson Lam <anson.lam94@gmail.com>
2019-07-07 16:36:22 -0700

    Configure client with heroku hosted server

--- src/client/src/config.js
+++ src/client/src/config.js
@@ -1,4 +1,5 @@
-export const BASE_URL = `http://localhost:3000/api/v1`;
+// export const BASE_URL = `http://localhost:3000/api/v1`;
+export const BASE_URL = `https://bcssttc-server.herokuapp.com/api/v1`;
 export const boysTeamStandings = [
   'Steveston-London A',
   'Kitsilano A',
@@ -20,7 +21,7 @@ export const boysTeamStandings = [
   'Vancouver Technical B',
   'J. N. Burnett B',
   'Steveston-London B',
-  'R. A. McMath B'
+  'R. A. McMath B',
 ];
 export const girlsTeamStandings = [
   'University Hill',
@@ -30,4 +31,15 @@ export const girlsTeamStandings = [
   'Burnaby North B',
   'Vancouver Technical A',
   'Vancouver Technical B',
+];
+export const order16 = [1, 6, 2, 5, 3, 4, 7, 0];
+export const advanceOrder8 = [
+  [0, 0], 
+  [3, 1], 
+  [2, 0],
+  [1, 1], 
+  [2, 1],
+  [1, 0],
+  [3, 0],
+  [0, 1],
 ];
